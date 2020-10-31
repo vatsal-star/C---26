680610cfa2b089ff94c03b00d781bd08c526dfe0
vatsal-star <72501613+vatsal-star@users.noreply.github.com>
2020-10-31 07:50:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+Kadai Paneer is a semi dry curry made with firm cottage cheese, onions, tomatoes,
+capsicum (green bell peppers) and freshly powdered kadai masala.
+This Kadai Paneer recipe tastes awesome and is truly a restaurant style recipe.
+▢5 to 6 kashmiri red chilies
+▢1.5 tablespoons coriander seeds
+3 to 4 tablespoons oil
+1 medium to large onion or 90 grams onions - finely chopped or ⅓ to ½ cup finely chopped onions
+2 teaspoons ginger-garlic paste
+5 to 6 medium tomatoes or 400 grams tomatoes - finely chopped or 2.5 to 3 cups finely chopped tomatoes
+1 large capsicum - julienned or sliced or ¾ to 1 cup thinly sliced capsicum (bell pepper)
+1 or 2 green chilies - slit
+½ cup water or add as required
+▢½ teaspoon garam masala powder
+250 grams paneer - cubed (cottage cheese)
+1 teaspoon crushed kasuri methi (dry fenugreek leaves)
+▢2 tablespoons chopped coriander leaves (cilantro leaves)
+1 inch ginger, julienne
+salt as required
